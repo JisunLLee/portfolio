5f31bb23a9dffb2bd6c6acb4524a51cf51632f38
lucialee <booungy@gmail.com>
2022-09-08 15:34:48 +0900

    로딩 애니메이션 좀 더 자연스럽게 변경

--- src/components/loading/home.jsx
+++ src/components/loading/home.jsx
@@ -6,9 +6,11 @@ const HomeLoading = () => {
   const [ani2, setAni2] = useState(2);
   const [ani3, setAni3] = useState(3);
   const [ani4, setAni4] = useState(4);
+  const [isShow, setIsShow] = useState(true);
   const circle = (prv) => (prv === 4 ? 1 : prv + 1);
 
   useEffect(() => {
+    setIsShow(true);
     const setCircle = setTimeout(() => {
       setAni1((prv) => circle(prv));
       setAni2((prv) => circle(prv));
@@ -16,13 +18,18 @@ const HomeLoading = () => {
       setAni4((prv) => circle(prv));
     }, 2000);
 
-    return () => clearTimeout(setCircle);
+    const setMainShow = setTimeout(() => {
+      setIsShow(false);
+    }, 1500);
+
+    return () => clearTimeout(setCircle, setMainShow);
   }, [ani1]);
 
   return (
     <div className={style.container}>
       <div className={style.showbox}>
         <div className={style.innerbox}>
+          <div className={isShow ? style.show : style.hide} />
           <div className={`${style['circle' + ani1]} ${style.study}`} />
           <div className={`${style['circle' + ani2]} ${style.drawing}`} />
           <div className={`${style['circle' + ani3]} ${style.climbing}`} />
--- src/routes/Test.jsx
+++ src/routes/Test.jsx
@@ -2,7 +2,13 @@ import HomeLoading from '../components/loading/home';
 
 export default function Test() {
   return (
-    <div style={{ position: 'relative' }}>
+    <div
+      style={{
+        position: 'relative',
+        width: '100%',
+        height: 'calc(100vh - 129px)',
+      }}
+    >
       <HomeLoading />
     </div>
   );
